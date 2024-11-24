ca81bc65c2d1c9810ff66cdef62db57700c61f20
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-11-24 22:11:44 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Footer from "./_component/Footer/Footer";
-// import Header from "./_component/Header/Header";
+import Header from "./_component/Header/Header";
 
 function App() {
   return (
